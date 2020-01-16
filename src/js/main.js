6b5669df4db89a26db6446c067519441eee1d928
Andry85 <shapovala@ukr.net>
2020-01-16 17:00:37 +0200

    added files

--- src/js/main.js
+++ src/js/main.js
@@ -33,6 +33,23 @@ $(function() {
 	/********************************************************/ 
 
 
+	/*********************************************************/
+						/* Setup thumbnails  */
+	/********************************************************/
+		var $easyzoom = $('.easyzoom').easyZoom();
+		var api1 = $easyzoom.filter('.easyzoom--with-thumbnails').data('easyZoom');
+
+		$('.thumbnails').on('click', 'a', function(e) {
+			var $this = $(this);
+			e.preventDefault();
+			// Use EasyZoom's `swap` method
+			api1.swap($this.data('standard'), $this.attr('href'));
+		});
+	/*********************************************************/
+						/* End Setup thumbnails  */
+	/********************************************************/ 
+
+
 
 
 
